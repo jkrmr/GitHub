9e04b9d21c92af73d9bf657a202015ef250277f6
Jake Romer <jkrmr.io@gmail.com>
2017-04-05 15:25:11 -0700

    Add Language to Repo table view cells

--- GitHub/Views/RepositoryTableCell.swift
+++ GitHub/Views/RepositoryTableCell.swift
@@ -12,30 +12,12 @@ class RepositoryTableCell: UITableViewCell {
   @IBOutlet weak var repoName: UILabel!
   @IBOutlet weak var repoDescription: UILabel!
   @IBOutlet weak var repoLanguage: UILabel!
-  @IBOutlet weak var starsContainer: UIView!
-  @IBOutlet weak var starsCount: UILabel!
-  @IBOutlet weak var forksContainer: UIView!
-  @IBOutlet weak var forksCount: UILabel!
 
   var repository: Repository! {
     didSet {
       repoName.text = repository.fullName
       repoDescription.text = repository.description
-//      repoLanguage.text = repository.language
-//
-//      if repository.stargazersCount > 0 {
-//        starsCount.text = "\(repository.stargazersCount)"
-//        starsContainer.isHidden = false
-//      } else {
-//        starsContainer.isHidden = true
-//      }
-//
-//      if repository.forksCount > 0 {
-//        forksCount.text = "\(repository.forksCount)"
-//        forksContainer.isHidden = false
-//      } else {
-//        forksContainer.isHidden = true
-//      }
+      repoLanguage.text = repository.language
     }
   }
 
